1f7cc66c1f72f95da8b112db08d2c557fda61659
nurulislamrimon <nurulislamrimon@gmail.com>
2024-11-12 16:35:31 +0600

    done

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const { WebSocketServer } = require("ws");
 // express server
 const app = express();
 // socket server
-const wss = new WebSocketServer({ port: 8080 });
+const wsServer = new WebSocketServer({ server: app });
 const port = 5000;
-
-console.log(wss);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+
+  // socket controller
+  wsServer.on("connection", (ws) => {
+    ws.on("message", (data) => {
+      console.log("received: %s", data);
+      ws.send(`Hello, you sent -> ${data}`);
+    });
+
+    ws.on("error", (error) => {
+      console.log("error: %s", error);
+    });
+
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
+
+    ws.send("Hi there, I am a WebSocket server");
+  });
 });
 
+// server listener
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
